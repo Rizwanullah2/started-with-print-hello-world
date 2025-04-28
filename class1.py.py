5f3dfb64b4bb5975e864e605cad9d6fb4aee359d
Rizwanullah2 <hafizrizwanullah2@gmail.com>
2025-04-28 18:58:26 +0500

    Add files via upload

---
+++ class1.py.py
@@ -0,0 +1,16 @@
+# #1st chapter
+# # built in function 
+# #screen par output dikane k liye 
+
+# #syntax 
+# print("hello world!") # output  hello world!
+
+# print("pakistan zindabad")               #output is pakistan zindabad 
+# print(12)                               
+# print(25.90)
+# print("90")
+# print("sir aneeq")
+
+
+
+
